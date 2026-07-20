a19d12ca1edf1911590cfef957f43359e70b0380
vandanamahant <vandanavandana924@gmail.com>
2026-07-20 16:15:25 +0530

    Update project files

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = CONFIG.WEATHER_API_KEY;
+const apiKey = typeof CONFIG !== 'undefined' ? CONFIG.WEATHER_API_KEY : "VERCEL_ENV_KEY";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const cityInput = document.getElementById("cityInput");
